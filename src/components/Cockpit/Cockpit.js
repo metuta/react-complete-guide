c7309585c8605d1b25d631e1ef54c4b649fefb63
Tugrul Tarhana <t.tarhana.extern@optica.de>
2020-11-26 13:55:06 +0300

    useEffect to react for certain or all changes

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
-    let assignedClasses = [];
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect only mount');
+
+        // setTimeout(() => {
+        //     alert('Saved data to cloud!');
+        // }, 1000);
+    }, []); // empty array -> runs only first time on mount, because there is no dependency to reload
+
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect only for props.persons');
+    }, [props.persons]); // would reload only, when persons-property changes!
+
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect only for props.show');
+    }, [props.show]); // would reload only, when show-property changes!
+
+    const assignedClasses = [];
+    const btnClass = [classes.Button];
 
     if (props.persons.length <= 2) {
         assignedClasses.push(classes.red);
@@ -10,8 +27,6 @@ const cockpit = (props) => {
     if (props.persons.length <= 1) {
         assignedClasses.push(classes.bold);
     }
-
-    let btnClass = [classes.Button];
 
     if (props.show) {
         btnClass.push(classes.Red);
