1ebc24f2378daa17c151ba97fbec3e0e521cafc7
Tugrul Tarhana <t.tarhana.extern@optica.de>
2020-11-27 23:37:48 +0300

    cleanup for MyBurger project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    
+    return (
+      <div>empy app</div>
+    );
+  }
+}
+
+export default App;
--- src/hoc/Auxiliary.js
+++
@@ -1,5 +0,0 @@
-// import React from 'react';  // this import is not neccessary, because no React-feature is used
-
-const auxiliary = props => props.children;  // auxiliary-component just returns adjacent elements as children
-
-export default auxiliary;
