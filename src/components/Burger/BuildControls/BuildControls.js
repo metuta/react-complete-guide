2708a6059f44e4340367ed9a4e08ea15907fc92e
Tugrul Tarhana <t.tarhana.extern@optica.de>
2020-11-28 21:07:16 +0300

    list-add-remove ingredients, purchase-cancel order

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import classes from './BuildControls.css';
+
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => {
+    const price = new Intl.NumberFormat('un-US', { style: 'currency', currency: 'USD' }).format(props.totalPrice);
+    return (
+        <div className={classes.BuildControls}>
+            <p>Current Price: <strong>{price}</strong></p>
+            {controls.map((control) => <BuildControl
+                added={() => props.ingredientAdded(control.type)}
+                removed={() => props.ingredientRemoved(control.type)}
+                key={control.label}
+                label={control.label}
+                disabled={props.disabled[control.type]}
+            />)}
+            <button
+                onClick={props.purchased}
+                disabled={props.addedCount === 0}
+                className={classes.OrderButton}>
+                ORDER NOW
+            </button>
+        </div>
+    );
+}
+
+export default buildControls;
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+  let ingredients = Object.keys(props.ingredients)
+    .map((ingredientKey) => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (ingredients.length === 0) {
+    ingredients = <p>Please add some ingredients!</p>;
+  }
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {ingredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
